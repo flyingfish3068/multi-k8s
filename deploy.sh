36585771ac92c55ce4df803000ab76b918585d1a
Guangping-Patrick Li <lgp_74@hotmail.com>
2020-01-13 15:51:22 -0800

    deploy.sh corrected_2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t patrick3068/multi-client:latest -t patrick3068/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t patrick3068/multi-server:latest -t patrick3068/multi-server:$SHA -f ./client/Dockerfile ./server
-docker build -t patrick3068/multi-worker:latest -t patrick3068/multi-worker:$SHA -f ./client/Dockerfile ./worker
+docker build -t patrick3068/multi-server:latest -t patrick3068/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t patrick3068/multi-worker:latest -t patrick3068/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push patrick3068/multi-client:latest
 docker push patrick3068/multi-server:latest
 docker push patrick3068/multi-worker:latest
@@ -8,6 +8,6 @@ docker push patrick3068/multi-client:$SHA
 docker push patrick3068/multi-server:$SHA
 docker push patrick3068/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=patrick3068/multi-server:$SHA
-kubectl set image deployments/client-deployment client=patrick3068/multi-client:$SHA
+kubectl set image deployments/client-deployment server=patrick3068/multi-client:$SHA
+kubectl set image deployments/server-deployment client=patrick3068/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=patrick3068/multi-worker:$SHA
